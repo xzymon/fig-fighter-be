0ff064c948d65c9de68e96b9d33126c9b033f576
xzymon <xzymon@gmail.com>
2023-02-25 18:19:35 +0100

    Project init, with Liquibase basic setup

---
+++ src/main/java/com/xzymon/ngsf/figfighter/FigFighterBeApplication.java
@@ -0,0 +1,13 @@
+package com.xzymon.ngsf.figfighter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FigFighterBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FigFighterBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xzymon/ngsf/figfighter/FigFighterBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xzymon.ngsf.figfighter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FigFighterBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
